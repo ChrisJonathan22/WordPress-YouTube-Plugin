d0c29fb214d1457a19c28298157c83bb07ab57f9
Chris Jonathan <christophereko@hotmail.fr>
2020-01-16 23:17:30 +0000

    I've added some content such as the plugin name, version etc...

--- wordpress-youtube-plugin.php
+++ wordpress-youtube-plugin.php
@@ -1,5 +1,12 @@
 <?php 
 
-defined( 'ABSPATH' ) or die( 'No script please!' );
+/* 
+Plugin Name: YouTube Subs
+Plugin URI: https://www.chrisjonathan.co.uk
+Description: Display YouTube sub button and count
+Version: 1.0.0
+Author: Chris Jonathan
+Author URI: https://www.chrisjonathan.co.uk
+*/
 
 ?>
