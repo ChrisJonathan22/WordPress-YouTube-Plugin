2b0369079bc64cf53ae3ef55a58d96ba04dcc962
Chris Jonathan <christophereko@hotmail.fr>
2020-01-19 21:15:19 +0000

    I've renamed the JS file from script.js to main.js and I've also loaded the CSS and JS file.

--- includes/youtubesubs-scripts.php
+++ includes/youtubesubs-scripts.php
@@ -2,10 +2,12 @@
 // Add Scripts
 function yts_add_scripts () {
     // Add Main CSS
-    wp_enqueue_style('yts-main-style', plugin_url() . '/youtubesubs/css/style.css');
+    wp_enqueue_style('yts-main-style', plugins_url() . '/WordPress-YouTube-Plugin/css/style.css');
     // Add Main JS
-    wp_enqueue_script('yts-main-script', plugin_url() . '/youtubesubs/js/script.js');
+    wp_enqueue_script('yts-main-script', plugins_url() . '/WordPress-YouTube-Plugin/js/main.js');
 }
 
+
+add_action('wp_enqueue_scripts', 'yts_add_scripts');
 
 ?>
--- wordpress-youtube-plugin.php
+++ wordpress-youtube-plugin.php
@@ -14,4 +14,7 @@ if(!defined('ABSPATH')) {
     exit;
 }
 
+// Load Scripts
+require_once(plugin_dir_path(__FILE__) . '/includes/youtubesubs-scripts.php');
+
 ?>
