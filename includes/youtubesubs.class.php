0ac5575ce478e40e8b88f810118c42c51879c13d
Chris Jonathan <christophereko@hotmail.fr>
2020-01-20 22:13:35 +0000

    I've added some code to display the widget.

---
+++ includes/youtubesubs.class.php
@@ -0,0 +1,34 @@
+<?php 
+
+
+
+
+/**
+ * Add a widget to the dashboard.
+ *
+ * This function is hooked into the 'wp_dashboard_setup' action below.
+ */
+
+
+function wporg_add_dashboard_widgets() {
+    wp_add_dashboard_widget(
+        'youtubesubs_widget',                          // Widget slug.
+        esc_html__( 'YouTube Subs', 'yts_domain' ), // Title.
+        'wporg_dashboard_widget_render'                    // Display function.
+    ); 
+}
+add_action( 'wp_dashboard_setup', 'wporg_add_dashboard_widgets' );
+
+/**
+ * Create the function to output the content of our Dashboard Widget.
+ */
+function wporg_dashboard_widget_render() {
+    // Display whatever you want to show.
+    esc_html_e( "Howdy! I'm a great Dashboard Widget.", "wporg" );
+}
+
+
+
+
+
+?>
